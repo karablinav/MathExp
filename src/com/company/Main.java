5d1464ebb5e0ab55fb29079b8aefd416d6643d48
karablinav <karablinav@gmail.com>
2015-04-14 08:35:52 +0300

    4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,46 +3,8 @@ package com.company;
 import java.util.*;
 
 public class Main {
-    public static int countNumb(String str, int i) {
-        while (str.charAt(i) >= '0' && str.charAt(i) <= '9' && Character.getNumericValue(str.charAt(i)) < (str.length() - 1) && i < (str.length()) - 1) {//&& str.charAt(i)<=str.length()
-            i++;
-        }
-        return i;
-    }
-
-    public static boolean checkNumb(String string) {
-        try {
-            Integer.parseInt(string);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-
-    public static byte priority(char ch) {
-        switch (ch) {
-            case '(':
-                return 0;
-            case ')':
-                return 0;
-            case '+':
-                return 2;
-            case '-':
-                return 2;
-            case '*':
-                return 3;
-            case '/':
-                return 3;
-            case '^':
-                return 4;
-            default:
-                return 6;
-        }
-    }
-
     public static void main(String[] args) {
-        Stack<Character> stack = new Stack<Character>();
+
         //String inputStr = new String("5-5");
         //String inputStr = new String("(5-5)");
         //String inputStr = new String("7-2*3");
@@ -52,76 +14,8 @@ public class Main {
         //String inputStr = new String("15/(25+4*3)");
         //String inputStr = new String("((5-5)-4");
         //String inputStr = new String("3+4*2/(1-5)^2");
-
-        //15/(25+4*3)
-        ArrayList<Character> operList = new ArrayList<Character>();
-        operList.add('+');
-        operList.add('-');
-        operList.add('*');
-        operList.add('/');
-        operList.add('(');
-        operList.add(')');
-        operList.add('^');
-        String outputStr = new String();
-
-        boolean flag = false;
-        int i = 0;
-
-        while (i < inputStr.length()) {
-            char c = inputStr.charAt(i);
-            if (checkNumb(String.valueOf(c))) {
-                while (!operList.contains(c)) {
-                    outputStr += String.valueOf(c);
-                    i++;
-
-                    if (inputStr.length() == i) {
-                        flag = true;
-                        break;
-                    }
-                    c = inputStr.charAt(i);
-                }
-                outputStr += ' ';
-            } else {
-                if (i != inputStr.length()) {
-                    if (operList.contains(c)) {
-                        if (c == ('(')) {
-                            stack.push(c);
-                            i++;
-                        } else if (c == ')') {
-                            outputStr += String.valueOf(stack.pop()) + ' ';
-                            if(stack.empty()){
-                                System.out.println("Ошибка");
-                                break;
-                            }
-                            char buf = stack.pop();
-
-                            while ((!stack.empty()) && buf != '(') {
-                                outputStr += String.valueOf(buf) + ' ';
-                                buf = stack.pop();
-                            }
-                            i++;
-                        } else {
-                            while ((!stack.empty()) && priority(c) <= priority(stack.peek())) {
-                                outputStr += String.valueOf(stack.pop()) + ' ';
-                            }
-                            stack.push(c);
-                            i++;
-                        }
-                    }
-                }
-            }
-        }
-
-        while (!stack.empty() && (stack.peek() != ('('))){
-            outputStr += String.valueOf(stack.pop()) + ' ';
-        }
-        String erorrs = new String();
-        if (!stack.empty()){
-            System.out.println("Ошибка!");
-            erorrs += String.valueOf(stack.pop()) + ' ';
-            System.out.println("Элементы в стеке: " + erorrs);
-        }
-        System.out.print(outputStr + erorrs);
+        MyMathExp myMathExp = new MyMathExp();
+        String exp = myMathExp.rpn(inputStr);
 
     }
 }
--- src/com/company/MyMathExp.java
+++ src/com/company/MyMathExp.java
@@ -1,6 +1,111 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class MyMathExp {
+
     public MyMathExp() {
+    }
+
+    private byte priority(char ch) {
+        switch (ch) {
+            case '(':
+                return 0;
+            case ')':
+                return 0;
+            case '+':
+                return 2;
+            case '-':
+                return 2;
+            case '*':
+                return 3;
+            case '/':
+                return 3;
+            case '^':
+                return 4;
+            default:
+                return 6;
+        }
+    }
+
+    private boolean checkNumb(String string) {
+        try {
+            Integer.parseInt(string);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public String rpn (String string){
+        String inputStr = string.replaceAll(" ", "");
+        Stack<Character> stack = new Stack<Character>();
+        ArrayList<Character> operList = new ArrayList<Character>();
+        operList.add('+');
+        operList.add('-');
+        operList.add('*');
+        operList.add('/');
+        operList.add('(');
+        operList.add(')');
+        operList.add('^');
+        String outputStr = new String();
+
+        int i = 0;
+        while (i < inputStr.length()) {
+            char c = inputStr.charAt(i);
+            if (checkNumb(String.valueOf(c))) {
+                while (!operList.contains(c)) {
+                    outputStr += String.valueOf(c);
+                    i++;
+
+                    if (inputStr.length() == i) {
+                        break;
+                    }
+                    c = inputStr.charAt(i);
+                }
+                outputStr += ' ';
+            } else {
+                if (i != inputStr.length()) {
+                    if (operList.contains(c)) {
+                        if (c == ('(')) {
+                            stack.push(c);
+                            i++;
+                        } else if (c == ')') {
+                            outputStr += String.valueOf(stack.pop()) + ' ';
+                            if(stack.empty()){
+                                 outputStr +="Ошибка";
+                                break;
+                            }
+                            char buf = stack.pop();
+
+                            while ((!stack.empty()) && buf != '(') {
+                                outputStr += String.valueOf(buf) + ' ';
+                                buf = stack.pop();
+                            }
+                            i++;
+                        } else {
+                            while ((!stack.empty()) && priority(c) <= priority(stack.peek())) {
+                                outputStr += String.valueOf(stack.pop()) + ' ';
+                            }
+                            stack.push(c);
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+        while (!stack.empty() && (stack.peek() != ('('))){
+            outputStr += String.valueOf(stack.pop()) + ' ';
+        }
+        String erorrs = new String();
+        if (!stack.empty()){
+            outputStr +="Ошибка";
+        }
+        return outputStr;
+    }
+
+    public void Calc(String exp){
+
     }
 }
